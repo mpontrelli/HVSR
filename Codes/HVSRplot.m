c21e6c51f0119d828a11cb18175cf31e82f15def
mpontrelli <marshallpontrelli@gmail.com>
2019-07-03 11:10:01 -0400

    Cleaned up the comments and transposed the trough matrices in taxstat section

--- Codes/HVSRplot.m
+++ Codes/HVSRplot.m
@@ -1,14 +1,11 @@
-function HVSRplot(ahatf, newfaxhz, sigma, confinthigh, confintlow, station)
+function HVSRplot(ahatf, newfaxhz, confinthigh, confintlow, station)
 
-newfaxhz=newfaxhz(10:length(newfaxhz)-1);
-confinthigh=confinthigh(10:length(confinthigh)-1);
-confintlow=confintlow(10:length(confintlow)-1);
+
 
 figure
 hold on
 confidenceinterval=shadedplot(newfaxhz, confinthigh, confintlow,[.9,.9,.9],'k');
 hold on
-ahatf=ahatf(10:length(ahatf)-1);
 ETF = plot(newfaxhz, ahatf, 'Color', [0.149 0.45098 0] , 'Linewidth', 1.5);
 title(strcat(station), 'FontSize', 20)
 xlabel('Frequency (Hz)','FontSize', 18)
