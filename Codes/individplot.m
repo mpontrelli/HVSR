7d7314234843107c76730ced861e025456fa8001
Pontrelli <mpontr01@tufts.edu>
2019-07-08 09:56:26 -0400

    Added a conditional statement that allows plotting all HVSRs individually. This is a function called individplot

---
+++ Codes/individplot.m
@@ -0,0 +1,10 @@
+function individplot(HV_final_matrix, newfaxhz, station)
+plot(newfaxhz, HV_final_matrix, 'Color',  'k' , 'Linewidth', .5);
+title(strcat(station), 'FontSize', 20)
+xlabel('Frequency (Hz)','FontSize', 18)
+ylabel('Amplification','FontSize', 18)
+set(gca,'FontSize',20,'YScale', 'log')
+xlim([0 5])
+ylim([0.1 100])
+grid on    
+end
