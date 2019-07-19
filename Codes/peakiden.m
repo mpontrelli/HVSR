a961708dab73e184c97a8307918023b59b66f5dc
jreyes08 <44142020+jreyes08@users.noreply.github.com>
2019-07-19 07:28:00 -0400

    Changed Peakiden, specratstat, and HVSR code

--- Codes/peakiden.m
+++ Codes/peakiden.m
@@ -1,53 +1,74 @@
-function [peakamp, peakfreq, amplocs2] = peakiden(ahatf, newfaxhz, lowbound, fsmin)
-N = length(ahatf)-lowbound; %length North_South_Component
-width = 2; %width for triangle moving average filter in hz
-q = ceil((fsmin/2-1)*width); %width for triangle moving average filter in samples
-e = smooth(ahatf, q, 'moving');
-e = e(lowbound: length(e));
+function [matrix,peakind,ahatf1,newfaxhz1] = peakiden(ahatf, newfaxhz, lowbound, fsmin)
+% N = length(ahatf)-lowbound; %length North_South_Component
+% width = 2; %width for triangle moving average filter in hz
+% q = ceil((fsmin/2-1)*width); %width for triangle moving average filter in samples
+% e = smooth(ahatf, q, 'moving');
+% e = e(lowbound: length(e));
 newfaxhz1 = newfaxhz(lowbound: length(newfaxhz));
+ahatf1 = ahatf(lowbound:length(ahatf));
 
 peakfreq = [];
 peakamp = []; 
 amplocs2 = [];
 %% Determine if peak is a peak
 count = 0;
-[amps,amplocs] = findpeaks(e);
-for hh = 1:length(amps)
-    freqpeak(hh) = newfaxhz1(amplocs(hh));
+[peaks,locs,w,p] = findpeaks(ahatf1,newfaxhz1);
+[B,I] = sort(p);
+for ii = 1:length(B)
+    if B(ii) > 1.5
+        o = ii;
+        break
+    end 
+end 
+B=B(o:end);
+w = w(I);
+topprom = B;
+topwidth = w(o:end);
+ind = I(o:end);
+A = peaks(ind); fn = locs(ind);
+[fn,I2] = sort(fn);
+A = A(I2); topprom = topprom(I2); topwidth = topwidth(I2);
+A=A'; topprom = topprom'; topwidth = topwidth';fn = fn';
+matrix = [fn,A,topprom, topwidth];
+for j = 1:length(fn)
+    peakind(j)=find(newfaxhz1==fn(j));
 end
-[trough,troughloc] = findpeaks(-1*e);
-for hh = 1:length(trough)
-    freqtrough(hh) = newfaxhz1(troughloc(hh));
-end
-if freqtrough(1) > freqpeak(1)
-    Y = 1;
-    X = 1;
-    trough = horzcat(Y,trough');
-    troughloc = horzcat(X,troughloc');
-end
-if freqpeak(length(freqpeak)) > freqtrough(length(freqtrough))
-    Y = -1;
-    X = newfaxhz1(length(newfaxhz1));
-    trough = horzcat(trough, Y);
-    troughloc = horzcat(troughloc,X);
-end
-for hh = 1:length(trough)
-    freqtrough(hh) = newfaxhz1(troughloc(hh));
-end
-for k = 1:length(amps)
-    height = amps(k)/sqrt(2);
-    lefttrough = -1 * trough(k);
-    righttrough = -1 * trough(k + 1);
-    if lefttrough < height && righttrough < height
-        count  = count + 1;
-        peakfreq(count) = newfaxhz1(amplocs(k));
-        peakamp(count) = amps(k); 
-        amplocs2(count) = amplocs(k);
-    end
-end
-figure 
-plot(newfaxhz1,e)
-hold on
-plot(newfaxhz1(amplocs),amps,'go')
+% for hh = 1:length(amps)
+%     freqpeak(hh) = newfaxhz1(amplocs(hh));
+% end
+% [trough,troughloc] = findpeaks(-1*e);
+% for hh = 1:length(trough)
+%     freqtrough(hh) = newfaxhz1(troughloc(hh));
+% end
+% if freqtrough(1) > freqpeak(1)
+%     Y = 1;
+%     X = 1;
+%     trough = horzcat(Y,trough');
+%     troughloc = horzcat(X,troughloc');
+% end
+% if freqpeak(length(freqpeak)) > freqtrough(length(freqtrough))
+%     Y = -1;
+%     X = newfaxhz1(length(newfaxhz1));
+%     trough = horzcat(trough, Y);
+%     troughloc = horzcat(troughloc,X);
+% end
+% for hh = 1:length(trough)
+%     freqtrough(hh) = newfaxhz1(troughloc(hh));
+% end
+% for k = 1:length(amps)
+%     height = amps(k)/sqrt(2);
+%     lefttrough = -1 * trough(k);
+%     righttrough = -1 * trough(k + 1);
+%     if lefttrough < height && righttrough < height
+%         count  = count + 1;
+%         peakfreq(count) = newfaxhz1(amplocs(k));
+%         peakamp(count) = amps(k); 
+%         amplocs2(count) = amplocs(k);
+%     end
+% end
+% figure 
+% plot(newfaxhz1,ahatf1)
+% hold on
+% plot(fn,A,'go')
 
 end
--- Codes/specratstat.m
+++ Codes/specratstat.m
@@ -1,16 +1,19 @@
-function [taxstat] = specratstat(peakamp, peakfreq, amplocs2, ahatf, newfaxhz, sigma, statname)
-taxstat = {};
-for f = 1:length(peakamp)
+function [taxstat] = specratstat(peakind,matrix, ahatf1, newfaxhz1, sigma, statname,lowbound)
+[m] = length(peakind);
+sigma1 = sigma(lowbound:length(sigma)); 
+sigvec = []; 
+for f = 1:m
+    loc = peakind(f);
+    disp(loc)
+    A = matrix(f,2);
+    disp(A)
+    [I1, I, f1, f2, hpb] =  HalfPowerBand2(A, loc, newfaxhz1, ahatf1);
     taxstat{f,1} = statname;
-    taxstat{f,2} = num2str(f);
-    taxstat{f,3} = num2str(peakfreq(f)); 
-    A = peakamp(f);
-    taxstat{f,4} = num2str(A);
-    amploc2 = amplocs2(f);
-    [I1, I2, f1, f2, hpb] =  HalfPowerBand2(A, amploc2, newfaxhz, ahatf); 
-    taxstat{f,5} = num2str(hpb);
-    a = sigma(I1:I2);
+    taxstat{f,2} = num2str(hpb);
+    a = sigma1(I1:I);
     sigmai = median(a);
-    taxstat{f,6} = num2str(sigmai);
+    taxstat{f,3} = num2str(sigmai);
 end
+matrix = num2cell(matrix);
+taxstat=[taxstat,matrix];
 end
