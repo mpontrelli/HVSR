f77c22bd436656c460e0ffb5314a3b59caebaff5
Pontrelli <mpontr01@tufts.edu>
2019-07-29 09:15:36 -0400

    changes to HVSRplot

--- Codes/HVSRplot.m
+++ Codes/HVSRplot.m
@@ -6,7 +6,7 @@ hold on
 ETF = plot(newfaxhz(lowbound:length(newfaxhz)), ahatf(lowbound:length(ahatf)), 'Color', [0.149 0.45098 0] , 'Linewidth', 1.5);
 title(strcat(statname, ' HVSR'), 'FontSize', 20)
 xlabel('Frequency (Hz)','FontSize', 18)
-ylabel('Amplification','FontSize', 18)
+ylabel('Ampli fication','FontSize', 18)
 set(gca,'FontSize',20,'YScale', 'log')
 xlim([newfaxhz(lowbound) 49])
 %ylim([0.1 100])
