4ca3a7f94c3214713bf387a37e6b73b468a3c714
Pontrelli <mpontr01@tufts.edu>
2019-10-02 12:33:46 -0400

    added pas2sac documentation

--- Codes/Reftek/runreftek.bat
+++ Codes/Reftek/runreftek.bat
@@ -1,1 +1,1 @@
-pas2sac C:\Users\Marshall\Desktop\141241005_002B51D8
+pas2sac C:\Users\mpontr01\Desktop\boston-site-response\field_deployments\radian_demo_deployment\potential_boreholes\DS26-60\Site_response\data\141241005_002B51D8 [C:\Users\mpontr01\Desktop] 
