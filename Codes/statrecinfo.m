3cd2cbeb407bef76b359f1aae70aa456f18ddbbb
Pontrelli <mpontr01@tufts.edu>
2019-10-24 15:36:38 -0400

    revamping comments. This push is on beginning of HVSR up to statrecinfo and statrecinfo

--- Codes/statrecinfo.m
+++ Codes/statrecinfo.m
@@ -1,3 +1,31 @@
+%% statrecinfo
+% statrec info is designed specifically for the Mexico City RACM dataset.
+% It loops through all the records at a station and pulls out the longest
+% record, shortest record and lowest sampling frequency used to record any
+% of the records. It is only needed if wavecut is toggled on. If you want
+% to use HVSR on another dataset and use wavecut, then you must reconfigure
+% statrecinfo to get the same information from the other dataset. Likely,
+% this just involves changing readfile1. It was developed in response to
+% questions over upsampling of data onto the longest record using linear 
+% interpolation.
+
+    % INPUTS
+    
+    % files - list of files in the folder
+    
+    % station - folder path that contains the waveform files
+    
+    %OUTPUTS
+    
+    % fsmin - lowest sampling frequency used to record any of the data
+    
+    % recmax = maximum number of samples in the recording
+    
+    % minimum number of samples in the recording
+    
+%% Author: Marshall Pontrelli
+% Date: developed summer, 2019 
+%% 
 function [fsmin, recmax, recmin] = statrecinfo(files, station)
 counter1 = 0;
 lengthvec = [];
