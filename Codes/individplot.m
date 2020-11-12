9c8ba16a6cd1a91e9a797ad7e6b6683de43e98ce
mpontrelli <marshallpontrelli@gmail.com>
2020-11-12 14:21:20 -0500

    edited individplot

--- Codes/individplot.m
+++ Codes/individplot.m
@@ -1,5 +1,5 @@
-function individplot(HV_final_matrix, newfaxhz, statname)
-figure
+function individplot(HV_final_matrix, newfaxhz, statname, sav,outpath)
+individualfiltered = figure;
 plot(newfaxhz, HV_final_matrix, 'Color',  'k' , 'Linewidth', .5);
 title(strcat(statname), 'FontSize', 20)
 xlabel('Frequency (Hz)','FontSize', 18)
@@ -7,5 +7,9 @@ ylabel('Amplification','FontSize', 18)
 set(gca,'YScale', 'log', 'XScale', 'log','FontName', 'Times New Roman', 'FontSize', 14)
 xlim([0 20])
 ylim([0.1 100])
-grid on    
+grid on   
+
+if strcmp(sav, 'yes') == 1
+    saveas(individualfiltered, strcat(outpath, '\', 'individualfiltered.jpg'));
+end
 end
